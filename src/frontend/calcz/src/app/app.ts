d62b1605256868bedf6b7b3eaa37d19d44ed3eab
Zantech <dev@zantech.com.br>
2025-11-05 09:49:34 -0300

    Criado o component de login e ajustes de design

--- src/frontend/calcz/src/app/app.ts
+++ src/frontend/calcz/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
