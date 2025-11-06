43eca98570d5e17ff7d9829e3e88264ce7ae1d0d
Zantech <dev@zantech.com.br>
2025-11-05 22:34:46 -0300

    Inicio da construção do component --Inicio--

--- src/frontend/calcz/src/app/inicio/inicio.ts
+++ src/frontend/calcz/src/app/inicio/inicio.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
-
+import { MenuSuperior } from '../menu-superior/menu-superior';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [MenuSuperior],
   template: `
+    <app-menu-superior></app-menu-superior>
     <p>
       inicio works!
     </p>
-    
+
   `,
   styleUrl: './inicio.css',
 })
