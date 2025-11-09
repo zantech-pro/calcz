124e305c411d20526e970af7ff0cdf4fccac8b34
Zantech <dev@zantech.com.br>
2025-11-09 14:42:14 -0300

    Terminado os templates de com tailwindCSS e Html de forma static, proxima etapa e adicionar a esses templates dinamismo

--- src/frontend/calcz/src/app/historico/historico.ts
+++ src/frontend/calcz/src/app/historico/historico.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
+import { MenuSuperior } from '../menu-superior/menu-superior';
 
 @Component({
   selector: 'app-historico',
-  imports: [],
-  template: `
-    <p>
-      historico works!
-    </p>
-  `,
+  imports: [MenuSuperior],
+  templateUrl: 'historico.html',
   styleUrl: './historico.css',
 })
 export class Historico {
